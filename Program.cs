6f9fe5118a9e2e68e952f8817d72dac6be2822da
Dieva Leon <58702512+dievaGit@users.noreply.github.com>
2020-04-19 14:59:07 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace Exercise_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaring variables
+            BigInteger F1 = 1, F2 = 1, result = 0;
+            int n = 0, k = 0;
+
+
+            //Requesting numerator and denominator
+            Console.Write(" Enter the numerator: ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            do
+            {
+                Console.Write(" Enter the denominator: ");
+                k = Convert.ToInt32(Console.ReadLine());
+            }
+            while (k > n);
+
+            //calculating the factorials
+            for (int i = n; i > 0; i--)
+            {
+                F1 *= i;
+            }
+
+            for (int i = n; i > 0; i--)
+            {
+                F1 *= i;
+            }
+            result = F1 * F2;
+
+            Console.Write(" N! * K! = {0}", result);
+
+            Console.ReadKey();
+
+        }
+    }
+}
